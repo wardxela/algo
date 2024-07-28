de2f35d3371af6cd0b4179d46e12c5c91dd3c755
wardxela <wardxela@gmail.com>
2024-07-29 00:11:17 +0300

    feat(advent-of-typescript): days 7..8

---
+++ advent-of-typescript/2023/test/08.test-d.ts
@@ -0,0 +1,29 @@
+import { expectTypeOf, test } from 'vitest'
+
+type RemoveNaughtyChildren<T> = {
+  [K in keyof T as K extends `naughty_${string}` ? never : K]: T[K]
+};
+
+test('Filtering The Children (part 2)', () => {
+  type SantasList = {
+    naughty_tom: { address: '1 candy cane lane' };
+    good_timmy: { address: '43 chocolate dr' };
+    naughty_trash: { address: '637 starlight way' };
+    naughty_candace: { address: '12 aurora' };
+  };
+  type test_wellBehaved_actual = RemoveNaughtyChildren<SantasList>;
+  type test_wellBehaved_expected = {
+    good_timmy: { address: '43 chocolate dr' };
+  };
+  expectTypeOf<test_wellBehaved_actual>().toEqualTypeOf<test_wellBehaved_expected>()
+
+  type Unrelated = {
+    dont: 'cheat';
+    naughty_play: 'fair';
+  };
+  type test_Unrelated_actual = RemoveNaughtyChildren<Unrelated>;
+  type test_Unrelated_expected = {
+    dont: 'cheat';
+  };
+  expectTypeOf<test_Unrelated_actual>().toEqualTypeOf<test_Unrelated_expected>()
+})
