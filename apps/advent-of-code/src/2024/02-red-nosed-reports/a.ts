08645d630bc6b8735bef56f0037bc5048d7dd306
wardxela <wardxela@gmail.com>
2024-12-02 20:02:20 +0300

    feat(advent-of-code): 2nd day of 2024

---
+++ apps/advent-of-code/src/2024/02-red-nosed-reports/a.ts
@@ -0,0 +1,38 @@
+export function solution(value: string): number {
+  const reports = value
+    .trim()
+    .split("\n")
+    .map((report) => report.split(" ").map((level) => Number.parseInt(level)));
+
+  let safeReports = 0;
+  for (const report of reports) {
+    if (isReportSafe(report)) {
+      safeReports++;
+    }
+  }
+
+  return safeReports;
+}
+
+function isReportSafe(report: number[]): boolean {
+  const firstDiff = report[1]! - report[0]!;
+  if (!isRangeSafe(firstDiff)) {
+    return false;
+  }
+  const isInc = firstDiff > 0;
+  for (let i = 1; i < report.length - 1; i++) {
+    const diff = report[i + 1]! - report[i]!;
+    if (!isRangeSafe(diff)) {
+      return false;
+    }
+    if ((isInc && diff < 0) || (!isInc && diff > 0)) {
+      return false;
+    }
+  }
+  return true;
+}
+
+function isRangeSafe(range: number): boolean {
+  const absRange = Math.abs(range);
+  return absRange >= 1 && absRange <= 3;
+}
